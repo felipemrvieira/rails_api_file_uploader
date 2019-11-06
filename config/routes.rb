4bfe4b3ecf7b4a13c7f80141c34a43be42d6564e
Felipe Maciel <felipemrvieira@gmail.com>
2019-11-06 14:39:55 -0300

    remove namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
-    resources :users, only: %i[create show index] do
-      get :avatar, on: :member
-    end
+  resources :users, only: %i[create show index] do
+    get :avatar, on: :member
   end
 end
