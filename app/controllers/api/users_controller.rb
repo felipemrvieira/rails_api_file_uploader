c1d5c416ad573034933aff4ac12f9d796a226aa6
Felipe Maciel <felipemrvieira@gmail.com>
2019-11-06 14:16:44 -0300

    Lista usuarios

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,47 +1,52 @@
 class Api::UsersController < ApplicationController
-    def create
-        user = User.new(create_params)
-        if user.save
-          render json: success_json(user), status: :created
-        else
-          render json: error_json(user), status: :unprocessable_entity
-        end
-    end
 
-    def show
-        user = User.find_by(id: params[:id])
-        if user.present?
-            render json: success_json(user), status: :ok
-        else
-            head :not_found
-        end
-    end
+  def index
+    render json: User.all
+  end
 
-    def avatar
-        user = User.find_by(id: params[:id])
-      
-        if user&.avatar&.attached?
-          redirect_to rails_blob_url(user.avatar)
-        else
+  def create
+      user = User.new(create_params)
+      if user.save
+        render json: success_json(user), status: :created
+      else
+        render json: error_json(user), status: :unprocessable_entity
+      end
+  end
+
+  def show
+      user = User.find_by(id: params[:id])
+      if user.present?
+          render json: success_json(user), status: :ok
+      else
           head :not_found
-        end
-    end
+      end
+  end
+
+  def avatar
+      user = User.find_by(id: params[:id])
     
-    private
-    def create_params
-        params.require(:user).permit(:username, :avatar)
-    end
-    
-    def success_json(user)
-        {
-          user: {
-            id: user.id,
-            username: user.username
-          }
+      if user&.avatar&.attached?
+        redirect_to rails_blob_url(user.avatar)
+      else
+        head :not_found
+      end
+  end
+  
+  private
+  def create_params
+      params.require(:user).permit(:username, :avatar)
+  end
+  
+  def success_json(user)
+      {
+        user: {
+          id: user.id,
+          username: user.username
         }
-    end
+      }
+  end
     
-    def error_json(user)
-        { errors: user.errors.full_messages }
-    end
+  def error_json(user)
+      { errors: user.errors.full_messages }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :users, only: %i[create show] do
+    resources :users, only: %i[create show index] do
       get :avatar, on: :member
     end
   end
